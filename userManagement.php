6fb0a87efe2bd3e95f9bd82576c48afd42827134
kevinloftus10 <kevinloftus10@gmail.com>
2021-04-29 17:41:52 -0400

    updated

--- userManagement.php
+++ userManagement.php
@@ -10,7 +10,11 @@ if($RequestMethod == "PUT") {
 
 // Get the User(s)
 if($RequestMethod == "GET") {
-    
+    if(isset($_GET['username'])) {
+        echo json_encode(GetUsers($_GET['username']));
+    }else {
+        echo json_encode(GetUsers(null));
+    }
 }
 
 // Update User
@@ -27,11 +31,44 @@ function SignUp( $obj ) {
 
 }
 
-function GetUsers() {
+function GetUsers($username) {
+    $statement = null;
+    
+    $db = OpenCon();
+    
+    if ($username == null){
+        $statement = $db->query("SELECT * FROM user_reg");
+    } else {
+        $statement = $db->query(sprintf("SELECT * FROM user_reg WHERE username = '%s'", $db->real_escape_string($username)));
+    }
+
+    CloseCon($db);
+
+    if(!$statement) {
+        return false;
+    }
+
+    $resultArray = array();
+    $index = 0;
+    while($row = $statement->fetch_object()) {
+        $tempArr = [];
+        $tempArr['username'] = $row->username;
+        $tempArr['name'] = $row->name;
+        $tempArr['email'] = $row->email;
+        $tempArr['phoneNumber'] = $row->phoneNumber;
+        $resultArray[$index] = $tempArr;
+        $index++;
+    }
+
+    return $resultArray;
 
 }
 
 function UpdateUser( $obj ) {
+
+    if( !isset($obj["username"]) || !isset($obj["name"]) || !isset($obj["password"]) || !isset($obj["email"]) || !isset($obj["phoneNumber"])){
+        return false;
+    }
 
 }
 
