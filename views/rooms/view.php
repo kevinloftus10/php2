01ec0ac158d98154ee2e58d46b0deb611464f4c3
kevinloftus10 <kevinloftus10@gmail.com>
2021-04-30 23:36:52 -0400

    view.php links added

--- views/rooms/view.php
+++ views/rooms/view.php
@@ -2,6 +2,28 @@
 
 include ("../../private/roomManagement.php");
 
+$RequestMethod = $_SERVER["REQUEST_METHOD"];
+$arrayToPrint = null;
 
+if($RequestMethod == "GET") {
+    $arrayToPrint = GetRoom(null);
+}
 
 ?>
+
+<html>
+    <head> </head>
+       <body>
+           <?php
+            foreach( $arrayToPrint as $val ) { 
+            echo $val["room_number"] . " location; " . $val["location"] . "</br>"; 
+            }
+           ?>
+            </br>
+            </br>
+            <a href="./add.php">Add Room</a></br>
+            <a href="./update.php">Update Room</a></br>
+            <a href="./delete.php">Delete Room</a></br>
+
+       </body> 
+</html>
