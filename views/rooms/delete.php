df7f2d12d78dfdcbbe8f917c0f9454305293034b
kevinloftus10 <kevinloftus10@gmail.com>
2021-04-30 23:46:08 -0400

    html

---
+++ views/rooms/delete.php
@@ -0,0 +1,25 @@
+<?php
+include ("../../private/roomManagement.php");
+
+
+?>
+
+<html>
+
+<head></head>
+<body>
+
+<h1>Which Room would you like to delete?</h1>
+
+<?php
+
+
+
+
+?>
+
+
+</body>
+
+
+</html>
